a7d56ddb4f7a10ed7083e2f60f61b052f151cb31
Roy <qq3392313023@163.com>
2023-11-18 13:59:21 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <RouterView/>
+</template>
+
+<style lang="scss">
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/views/admin/admin.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="roy_admin">
+        <aside>
+            <i class="iconfont icon-shezhi"></i>
+        </aside>
+        <div class="main">
+        <header>
+            <div class="left">
+                <a-breadcrumb>
+                    <a-breadcrumb-item>首页</a-breadcrumb-item>
+                    <a-breadcrumb-item><a href="#">个人中心</a></a-breadcrumb-item>
+                    <a-breadcrumb-item>用户列表</a-breadcrumb-item>
+                </a-breadcrumb>
+            </div>
+            <div class="right"></div>
+        </header>
+        <div class="tabs"></div>
+        <main></main>
+    </div>
+    </div>
+    
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+  .roy_admin {
+    width: 100%;
+    display: flex;
+    aside {
+        width: 240px;
+        height: 100vh;
+        background-color: cadetblue;
+    }
+    .main {
+        width: calc(100% - 240px);
+        header {
+            height: 60px;
+            background-color: white;
+            padding:0 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .tabs {
+            height: 30px;
+            border: 1px solid #f0eeee;
+        }
+        main {
+           background-color: #f0eeee; 
+           height: calc(100vh - 90px);
+        }
+    }
+  }
+</style>
