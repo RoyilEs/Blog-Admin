a3a96e01e07545035547a497c4c7e7cd00af6066
Roy <qq3392313023@163.com>
2023-11-19 19:32:18 +0800

    用户顶部模块

---
+++ src/components/roy_user_info.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="roy_user_info">
+    <div class="avatar">
+      <img src="/src/assets/img/头像.jpg" alt="头像">
+    </div>
+
+    <div class="drop_menu">
+      <a-dropdown>
+        <a class="ant-dropdown-link" @click.prevent>
+          Hover me
+          <i class="fa fa-angle-down"></i>
+        </a>
+        <template #overlay>
+          <a-menu @click="menuClick">
+            <a-menu-item key="user_center">
+              <a href="javascript:;">个人中心</a>
+            </a-menu-item>
+            <a-menu-item key="article_list">
+              <a href="javascript:;">文章列表</a>
+            </a-menu-item>
+            <a-menu-item key="logout">
+              <a href="javascript:;">注销退出</a>
+            </a-menu-item>
+          </a-menu>
+        </template>
+      </a-dropdown>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+function menuClick({key}) {
+  //退出操作 其他跳转路由
+  if (key === "logout") {
+    console.log("logout")
+    return
+  }
+  router.push({
+    name: key
+  })
+}
+</script>
+
+<style>
+  .roy_user_info {
+    display: flex;
+    align-items: center;
+  }
+</style>
--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -18,31 +18,7 @@
                   <i v-else class="fa fa-moon-o" @click="setTheme"></i>
                   <i class="fa fa-arrows-alt"></i>
                 </div>
-                <div class="avatar">
-                  <img src="/src/assets/img/头像.jpg" alt="头像">
-                </div>
-
-                <div class="drop_menu">
-                  <a-dropdown>
-                    <a class="ant-dropdown-link" @click.prevent>
-                      Hover me
-                      <i class="fa fa-angle-down"></i>
-                    </a>
-                    <template #overlay>
-                      <a-menu @click="menuClick">
-                        <a-menu-item key="user_center">
-                          <a href="javascript:;">个人中心</a>
-                        </a-menu-item>
-                        <a-menu-item key="article_list">
-                          <a href="javascript:;">文章列表</a>
-                        </a-menu-item>
-                        <a-menu-item key="logout">
-                          <a href="javascript:;">注销退出</a>
-                        </a-menu-item>
-                      </a-menu>
-                    </template>
-                  </a-dropdown>
-                </div>
+                  <RoyUserInfo></RoyUserInfo>
               </div>
         </header>
         <div class="tabs"></div>
@@ -54,22 +30,10 @@
 
 <script setup>
 import RoyAside from "@/components/admin/admin_aside.vue";
+import RoyUserInfo from "@/components/roy_user_info.vue";
 
-import {useRouter} from "vue-router";
 import {ref} from "vue";
 
-const router = useRouter();
-
-function menuClick({key}) {
-    //退出操作 其他跳转路由
-    if (key === "logout") {
-      console.log("logout")
-      return
-    }
-    router.push({
-      name: key
-    })
-  }
 
   const theme = ref(true)
 
